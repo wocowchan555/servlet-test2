d35e1417a0246666fb090e7c459a5b0691015078
David <david.chan@sagetech.com.hk>
2019-01-17 17:36:34 +0800

    viki cry a lot

--- src/main/java/com/example/on1/Test.java
+++ src/main/java/com/example/on1/Test.java
@@ -25,7 +25,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Good Morning World!");
+		response.getWriter().append("Viki cry");
 	}
 
 	/**
