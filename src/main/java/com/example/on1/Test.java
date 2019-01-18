244035c15d5d77e84a14278126f6b907d570c290
David <david.chan@sagetech.com.hk>
2019-01-18 09:32:27 +0800

    long cry a lot too

--- src/main/java/com/example/on1/Test.java
+++ src/main/java/com/example/on1/Test.java
@@ -25,7 +25,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Viki cry");
+		response.getWriter().append("Viki cry and Long cry");
 	}
 
 	/**
